9d91ebe6efd1c831aa3adc576638ee6f40cce442
Alex Tamayo <catd00@gmail.com>
2023-07-29 05:16:46 -0700

    Modify SQL file to help with the database.js queries.

--- 1_queries/3_property_listings_by_city.sql
+++ 1_queries/3_property_listings_by_city.sql
@@ -19,3 +19,29 @@ ORDER BY
 properties.cost_per_night
 LIMIT
 10;
+
+
+SELECT
+properties.*,
+avg(property_reviews.rating) as average_rating
+FROM
+properties
+JOIN
+property_reviews ON properties.id = property_id
+WHERE
+city LIKE '%a%'
+AND
+cost_per_night >= 40000
+GROUP BY
+properties.id
+ORDER BY
+properties.cost_per_night
+LIMIT
+10
+;
+
+SELECT owner_id, COUNT(*) as number_of_listings
+FROM properties
+GROUP BY owner_id
+HAVING COUNT(*) > 4
+ORDER BY owner_id DESC;
--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -22,3 +22,23 @@ ORDER BY
 reservations.start_date
 LIMIT
 10;
+
+
+SELECT
+  properties.*,
+  avg(property_reviews.rating) as average_rating
+FROM
+  reservations
+JOIN
+  properties ON properties.id = reservations.property_id
+JOIN
+  property_reviews ON properties.id = property_reviews.property_id
+WHERE
+  reservations.guest_id = 850
+GROUP BY
+  properties.id,
+  reservations.start_date
+ORDER BY
+  reservations.start_date
+LIMIT
+  10;
