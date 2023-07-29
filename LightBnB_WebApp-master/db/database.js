348860872a5a21d1b68e62e5188b873bc2889a42
Alex Tamayo <catd00@gmail.com>
2023-07-29 00:50:59 -0700

    Change default parametres to use DB

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,6 +1,15 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
 /// Users
 
 /**
@@ -8,15 +17,35 @@ const users = require("./json/users.json");
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+// const getUserWithEmail = function (email) {
+//   let resolvedUser = null;
+//   for (const userId in users) {
+//     const user = users[userId];
+//     if (user?.email.toLowerCase() === email?.toLowerCase()) {
+//       resolvedUser = user;
+//     }
+//   }
+//   return Promise.resolve(resolvedUser);
+// };
+
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user?.email.toLowerCase() === email?.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  const queryStr = `
+  SELECT *
+  FROM
+    users
+  WHERE
+    email = $1;    
+  `;
+
+  const values = [email];
+
+  return pool
+  .query(queryStr, values)
+  .then(result => {
+    if (!result.rows[0]) return null;
+    return result.rows[0]
+  })
+  .catch(err => console.log(err.message)) 
 };
 
 /**
@@ -24,8 +53,28 @@ const getUserWithEmail = function (email) {
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+// const getUserWithId = function (id) {
+//   return Promise.resolve(users[id]);
+// };
+
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  const queryStr = `
+  SELECT *
+  FROM
+    users
+  WHERE
+    id = $1;    
+  `;
+
+  const values = [id];
+
+  return pool
+  .query(queryStr, values)
+  .then(result => {
+    if (!result.rows[0]) return null;
+    return result.rows[0]
+  })
+  .catch(err => console.log(err.message)) 
 };
 
 /**
@@ -33,11 +82,29 @@ const getUserWithId = function (id) {
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
+// const addUser = function (user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// };
+
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+
+  const queryStr = `
+  INSERT INTO
+    users (name, email, password)
+  VALUES
+    ($1, $2, $3)
+  RETURNING *;
+  `;
+
+  const values = [user.name, user.email, user.password];
+
+  return pool
+  .query(queryStr, values)
+  .then(result => result.rows[0])
+  .catch(err => console.log(err.message)) 
 };
 
 /// Reservations
@@ -59,13 +126,31 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+// const getAllProperties = function (options, limit = 10) {
+//   const limitedProperties = {};
+//   for (let i = 1; i <= limit; i++) {
+//     limitedProperties[i] = properties[i];
+//   }
+//   return Promise.resolve(limitedProperties);
+// };
+const getAllProperties = (options, limit = 10) => {
+
+  const queryStr = `
+  SELECT *
+  FROM
+    properties
+  LIMIT
+    $1;    
+  `;
+
+  const values = [limit];
+
+  return pool
+    .query(queryStr, values)
+    .then(result => result.rows)
+    .catch(err => console.log(err.message));
 };
+
 
 /**
  * Add a property to the database
